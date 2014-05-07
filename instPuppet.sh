5131c71e6b4b7d6c1efd1376d99faff1af9251e0
Matan Kaminski <matan.kaminski.sdjasf@gmail.com>
2014-05-07 15:34:04 -0400

    Small Script to install and configure puppet to run on boot and look to correct master. Modify Puppet.conf in files to change what master puppet looks to.

---
+++ instPuppet.sh
@@ -0,0 +1,5 @@
+sudo dpkg -i puppetlabs-release-precise.deb
+sudo apt-get update -y
+sudo apt-get install puppet -y
+mv -f files/puppet.conf /etc/puppet/puppet.conf
+mv -f files/puppet /etc/default/puppet
